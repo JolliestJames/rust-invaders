0a796cbf14a8330a24d64ffc3e3cf656d5ad47ac
jame <cloudismydog@gmail.com>
2023-11-07 11:56:24 -0800

    move primary window to top corner for development

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![allow(unused)] // silence unused warnings while exploring 
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+
+// region:      --- Asset Constants
+
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+
+// endregion:   --- Asset Constants
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                resolution: (1280.0, 720.0).into(),
+                // position:
+                resolution: (600., 320.).into(),
                 title: "Rust Invaders!".to_string(),
                 ..default()
             }),
@@ -17,16 +25,26 @@ fn main() {
         .run()
 }
 
-fn setup_system(mut commands: Commands) {
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+) {
     // add 2D camera
     commands.spawn(Camera2dBundle::default());
 
+    // capture window size
+    let Ok(mut primary) = primary_query.get_single_mut() else {
+        return;
+    };
+    let (win_w, win_h) = (primary.width(), primary.height());
+
+    // position window for development
+    primary.position = WindowPosition::At(IVec2::new(2100, 0));
+
+    // add player
     commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(150., 150.)),
-            ..default()
-        },
+        texture: asset_server.load(PLAYER_SPRITE),
         ..default()
     });
 }
