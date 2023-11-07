be52ae59648dbdb8bc0d548873ad7c911c455f5c
jame <cloudismydog@gmail.com>
2023-11-07 09:49:00 -0800

    draw window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(unused)] // silence unused warnings while exploring 
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (1280.0, 720.0).into(),
+                title: "Rust Invaders!".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run()
 }
