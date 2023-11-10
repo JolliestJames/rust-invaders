f06d6885c791aa58a3a49244cfdce73548ac6437
jame <cloudismydog@gmail.com>
2023-11-10 07:27:05 -0800

    scale enemy sprite and perform random translation

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,5 +1,7 @@
-use crate::{GameTextures, SPRITE_SCALE};
+use crate::components::{Enemy, Movable, Velocity, SpriteSize};
+use crate::{GameTextures, WinSize, SPRITE_SCALE, ENEMY_SIZE};
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
 pub struct EnemyPlugin;
 
@@ -7,16 +9,29 @@ impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PostStartup, enemy_spawn_system);
     }
-
 }
 
-fn enemy_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>) {
-    commands.spawn(SpriteBundle {
-        texture: game_textures.enemy.clone(),
-        transform: Transform {
-            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+fn enemy_spawn_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    win_size: Res<WinSize>,
+) {
+    let mut rng = thread_rng();
+    let w_span = win_size.w / 2. - 100.;
+    let h_span = win_size.h / 2. - 100.;
+    let x = rng.gen_range(-w_span..w_span);
+    let y = rng.gen_range(-h_span..h_span);
+
+    commands
+        .spawn(SpriteBundle {
+            texture: game_textures.enemy.clone(),
+            transform: Transform {
+                translation: Vec3::new(x, y, 10.),
+                scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(Enemy)
+        .insert(SpriteSize::from(ENEMY_SIZE));
 }
