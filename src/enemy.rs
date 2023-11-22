a39fb8e9ead2913c124dc6014363cadbad607e25
jame <cloudismydog@gmail.com>
2023-11-22 13:01:45 -0800

    spawn no more than specified count of max enemies, implement enemy fire system

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,37 +1,74 @@
-use crate::components::{Enemy, Movable, Velocity, SpriteSize};
-use crate::{GameTextures, WinSize, SPRITE_SCALE, ENEMY_SIZE};
+use std::time::Duration;
+
+use crate::components::{Enemy, Movable, Velocity, SpriteSize, Laser, FromEnemy};
+use crate::{GameTextures, WinSize, EnemyCount, SPRITE_SCALE, ENEMY_SIZE, ENEMY_LASER_SIZE, ENEMY_MAX};
 use bevy::prelude::*;
+use bevy::time::common_conditions::on_timer;
 use rand::{thread_rng, Rng};
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, enemy_spawn_system);
+        // app.add_systems(Update, enemy_spawn_system);
+        app
+            .add_systems(Update, enemy_spawn_system.run_if(on_timer(Duration::from_secs_f32(1.))))
+            .add_systems(Update, enemy_fire_system);
     }
 }
 
 fn enemy_spawn_system(
     mut commands: Commands,
     game_textures: Res<GameTextures>,
+    mut enemy_count: ResMut<EnemyCount>,
     win_size: Res<WinSize>,
 ) {
-    let mut rng = thread_rng();
-    let w_span = win_size.w / 2. - 100.;
-    let h_span = win_size.h / 2. - 100.;
-    let x = rng.gen_range(-w_span..w_span);
-    let y = rng.gen_range(-h_span..h_span);
+    if enemy_count.0 < ENEMY_MAX {
+        let mut rng = thread_rng();
+        let w_span = win_size.w / 2. - 100.;
+        let h_span = win_size.h / 2. - 100.;
+        let x = rng.gen_range(-w_span..w_span);
+        let y = rng.gen_range(-h_span..h_span);
 
-    commands
-        .spawn(SpriteBundle {
-            texture: game_textures.enemy.clone(),
-            transform: Transform {
-                translation: Vec3::new(x, y, 10.),
-                scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+        commands
+            .spawn(SpriteBundle {
+                texture: game_textures.enemy.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 10.),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                    ..default()
+                },
                 ..default()
-            },
-            ..default()
-        })
-        .insert(Enemy)
-        .insert(SpriteSize::from(ENEMY_SIZE));
+            })
+            .insert(Enemy)
+            .insert(SpriteSize::from(ENEMY_SIZE));
+
+        enemy_count.0 += 1;
+        }
+}
+
+fn enemy_fire_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+) {
+    for &tf in enemy_query.iter() {
+        let (x, y) = (tf.translation.x, tf.translation.y);
+
+        commands
+            .spawn(SpriteBundle {
+                texture: game_textures.enemy_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y - 15., 0.),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(Laser)
+            .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+            .insert(FromEnemy)
+            .insert(Movable { auto_despawn: true })
+            .insert(Velocity { x: 0., y: -1. });
+    }
 }
