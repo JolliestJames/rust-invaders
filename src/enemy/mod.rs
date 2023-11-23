956fcf8065f8792b33c5008f2ae0b266784988e7
jame <cloudismydog@gmail.com>
2023-11-23 06:23:15 -0800

    refactor, use formation to specify enemy movement

--- src/enemy/mod.rs
+++ src/enemy/mod.rs
@@ -9,11 +9,17 @@ use bevy::prelude::*;
 use bevy::time::common_conditions::on_timer;
 use rand::{thread_rng, Rng};
 
+use self::formation::{FormationMaker, Formation};
+
+mod formation;
+
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
+        app
+            .insert_resource(FormationMaker::default())
+            .add_systems(
             Update,
             (
                 enemy_spawn_system.run_if(on_timer(Duration::from_secs_f32(1.))),
@@ -28,14 +34,12 @@ fn enemy_spawn_system(
     mut commands: Commands,
     game_textures: Res<GameTextures>,
     mut enemy_count: ResMut<EnemyCount>,
+    mut formation_maker: ResMut<FormationMaker>,
     win_size: Res<WinSize>,
 ) {
     if enemy_count.0 < ENEMY_MAX {
-        let mut rng = thread_rng();
-        let w_span = win_size.w / 2. - 100.;
-        let h_span = win_size.h / 2. - 100.;
-        let x = rng.gen_range(-w_span..w_span);
-        let y = rng.gen_range(-h_span..h_span);
+        let formation = formation_maker.make(&win_size);
+        let (x, y) = formation.start;
 
         commands
             .spawn(SpriteBundle {
@@ -48,6 +52,7 @@ fn enemy_spawn_system(
                 ..default()
             })
             .insert(Enemy)
+            .insert(formation)
             .insert(SpriteSize::from(ENEMY_SIZE));
 
         enemy_count.0 += 1;
@@ -90,33 +95,41 @@ fn enemy_fire_system(
 
 fn enemy_movement_system(
     time: Res<Time>,
-    mut query: Query<&mut Transform, With<Enemy>>
+    mut query: Query<(&mut Transform, &mut Formation), With<Enemy>>
 ) {
     let now = time.elapsed_seconds_f64() as f32;
 
-    for mut transform in query.iter_mut() {
+    for (mut transform, mut formation) in query.iter_mut() {
         let (x_org, y_org) = (transform.translation.x, transform.translation.y);
 
-        let max_distance = TIME_STEP * BASE_SPEED;
+        let max_distance = TIME_STEP * formation.speed;
 
-        let dir: f32 = -1.; // 1 for counter clockwise, -1 clockwise
-        let (x_pivot, y_pivot) = (0., 0.);
-        let (x_radius, y_radius) = (200., 130.);
+        // 1 for counter clockwise, -1 clockwise
+        let dir: f32 = if formation.start.0 < 0. { 1. } else { -1. };
+        let (x_pivot, y_pivot) = formation.pivot;
+        let (x_radius, y_radius) = formation.radius;
 
-        let angle = dir * BASE_SPEED * TIME_STEP * now % 360. / PI;
+        let angle = formation.angle + dir * formation.speed * TIME_STEP / (x_radius.min(y_radius) * PI / 2.);
 
         let x_distance = x_radius * angle.cos() + x_pivot;
         let y_distance = y_radius * angle.sin() + x_pivot;
 
+        // compute distance
         let dx = x_org - x_distance;
         let dy = y_org - y_distance;
         let distance = (dx * dx + dy * dy).sqrt();
         let distance_ratio = if distance != 0. { max_distance / distance } else { 0. };
 
+        // compute final position
         let x = x_org - dx * distance_ratio;
         let x = if dx > 0. { x.max(x_distance) } else { x.min(x_distance) };
         let y = y_org - dy * distance_ratio;
         let y = if dy > 0. { y.max(y_distance) } else { y.min(y_distance) };
+
+        // begin rotation the formation angle
+        if distance < max_distance * formation.speed / 20. {
+            formation.angle = angle;
+        }
 
         let translation = &mut transform.translation;
         (translation.x, translation.y) = (x, y);
